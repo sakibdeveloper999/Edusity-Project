af73978378383adc487c731bc986e5bc8c25c314
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-12-15 21:25:46 +0600

    Fix import path for VideoPlayer component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
-import VideoPlayer from './components/videoPlayer/VideoPlayer'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 
 
 const App = () => {
